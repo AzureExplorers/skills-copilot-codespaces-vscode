e6aee5a248716638b98e68b074a03a08531f715f
Naga Jagadeesh Budha <67053127+AzureExplorers@users.noreply.github.com>
2025-01-10 05:15:10 +0000

    updated comments

--- comments.js
+++ comments.js
@@ -1,20 +1,3 @@
-// Create a web server that listens on port 3000 and serves the comments.html file.
-
-const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.readFile('comments.html', (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write('File not found!');
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-});
-
-server.listen(3000);
-console.log('Server listening on port 3000');
+// Create a web server that listens on port 3000 and serves the following
+// JSON data when it receives a GET request to the path '/comments':
+//
